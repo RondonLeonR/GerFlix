1d9c50736d81eb0244ef7a352ed96e34e552d715
RondonLeonR <36230656+RondonLeonR@users.noreply.github.com>
2018-04-27 21:30:23 -0300

    Add files via upload

---
+++ GerFlix-master/GerFlix-master/GerFlix 1.0/Serie.c
@@ -0,0 +1,75 @@
+
+#include "Serie.h"
+#include <string.h>
+#include <stdio.h>
+
+/*
+    int idSerie;
+    char nombre[50];
+    char genero[20];
+    int cantidadTemporadas;
+    int estado;
+
+*/
+
+void inicializarSeriesEstado(eSerie series[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        series[i].estado = 0;
+    }
+}
+
+
+void inicializarSeriesHardCode(eSerie series[])
+{
+    int id[5] = {100,101,102,103,104};
+    char nombre[][50]={"TBBT","TWD","GOT","BB","LCDP"};
+    char genero[][50]={"Comedia","Terror","Suspenso","Policial","Policial"};
+    int cantidad[5] = {10,9,7,5,2};
+
+    int i;
+
+    for(i=0; i<5; i++)
+    {
+        series[i].idSerie=id[i];
+        series[i].cantidadTemporadas=cantidad[i];
+        series[i].estado = 1;
+        strcpy(series[i].nombre, nombre[i]);
+        strcpy(series[i].genero, genero[i]);
+    }
+}
+
+void mostrarListaSeries(eSerie listado[], int l)
+{
+    int i;
+    printf("\nSeries: \n");
+    for(i=0;i<l;i++)
+    {
+        if(listado[i].estado==1)
+        {
+            printf("%d %s %s %d\n",listado[i].idSerie,listado[i].nombre,listado[i].genero,listado[i].cantidadTemporadas);
+
+        }
+    }
+}
+
+/*void mostrarSerieConSuUsuario(eSerie serie[], int l,eUsuario usuario[], int r)
+{
+    for(int i=0;i<l;i++)
+    {
+        for(int j=0;j<r;j++)
+        {
+            if(serie[i].idSerie==usuario[j].idSerie&&serie[i].estado==1)
+            {
+                printf("%s %s %s",serie[i].nombre,serie[i].genero,usuario[j].nombre);
+            }
+        }
+    }
+}*/
+
+
+
+
---
+++ GerFlix-master/GerFlix-master/GerFlix 1.0/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Usuario.h"
+#define TAMSERIE 20
+#define TAMUSUARIO 100
+
+
+
+int main()
+{
+    int option;
+    char resp='s';
+
+    eSerie listaDeSeries[TAMSERIE];
+    eUsuario listaDeUsuarios[TAMUSUARIO];
+
+    inicializarSeriesEstado(listaDeSeries, TAMSERIE);
+    inicializarSeriesHardCode(listaDeSeries);
+
+    inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
+    inicializarUsuariosHardCode(listaDeUsuarios);
+
+    do{
+    system("cls");
+    printf("1. Mostrar el listado de series\n");
+    printf("2. Mostrar el listado de usuarios\n");
+    printf("3. Mostrar el listado de Usuarios con el nombre de la serie que ve.\n");
+    printf("4. Mostrar por cada serie, el nombre de los usuarios que la ven.\n");
+    printf("5. Salir.\n");
+    scanf("%d",&option);
+
+    switch(option){
+        case 1:{
+        mostrarListaSeries(listaDeSeries, TAMSERIE);
+        getche();
+        break;
+        }
+        case 2:{
+        mostrarListaUsuarios(listaDeUsuarios, TAMUSUARIO);
+        getche();
+        break;
+        }
+        case 3:{
+        mostrarUsuarioConSuSerie(listaDeUsuarios,TAMUSUARIO,listaDeSeries,TAMSERIE);
+        getche();
+        break;
+        }
+        case 4:{
+        //mostrarSerieConSuUsuario(listaDeSeries,TAMSERIE,listaDeUsuarios,TAMUSUARIO);
+        break;
+        }
+        case 5:{
+        resp = 'n';
+        break;
+        }
+
+    }
+
+
+    }while(resp=='s');
+
+    return 0;
+}
